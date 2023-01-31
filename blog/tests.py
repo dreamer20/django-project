5955d351b35ffdf19c3ba49e9e2fbaf06cacfbc4
dreamer20 <gv4alex@gmail.com>
2023-01-31 11:24:33 +0300

    Initial commit

---
+++ blog/tests.py
@@ -0,0 +1,9 @@
+from django.test import TestCase
+from django.urls import reverse
+# Create your tests here.
+
+
+class RegisterViewTest(TestCase):
+    def test_register_view(self):
+        response = self.client.get(reverse('blog:register'))
+        self.assertEqual(response.status_code, 200)
