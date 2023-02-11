b8ad6a5663fa104220f8197f0c5df2ac22c7469b
dreamer20 <gv4alex@gmail.com>
2023-02-11 10:14:42 +0300

    Rename model field from pud_date to pub_date

--- blog/models.py
+++ blog/models.py
@@ -3,7 +3,7 @@ from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from taggit.managers import TaggableManager
 from tinymce import models as tinymce_models
-from datetime import datetime
+from django.utils import timezone
 # Create your models here.
 
 
@@ -15,7 +15,7 @@ class Article(models.Model):
     title = models.CharField(max_length=300, default='Title')
     preview = tinymce_models.HTMLField(default='')
     content = tinymce_models.HTMLField(default='')
-    pud_date = models.DateTimeField(default=datetime.now)
+    pub_date = models.DateTimeField(default=timezone.now)
     hidden = models.BooleanField(default=False)
     tags = TaggableManager()
     author = models.ForeignKey(
