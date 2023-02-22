2c5f4672481e35573d6e251dd8ebb88b8cd09668
dreamer20 <gv4alex@gmail.com>
2023-02-22 18:10:07 +0300

    Script update for initial start

--- start.sh
+++ start.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 if [ "$initial" = true ]; then
+    echo "Start database migration."
     python3 manage.py migrate
-    python3 manage.py runserver 0.0.0.0:8000
-else
-    python3 manage.py runserver 0.0.0.0:8000
+    until python3 manage.py migrate
+    do
+        echo "Wait 3 seconds to try migration again."
+        sleep 3
+    done
 fi
+python3 manage.py runserver 0.0.0.0:8000
