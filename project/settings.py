ae12033843f384116c277becfea8a9e415709ad7
dreamer20 <gv4alex@gmail.com>
2023-03-12 20:35:14 +0300

    Change email backend to smtp server.

--- project/settings.py
+++ project/settings.py
@@ -136,7 +136,7 @@ MEDIA_URL = 'blog/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # STMP server settings
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.yandex.ru'
 EMAIL_PORT = 465
 EMAIL_HOST_PASSWORD = 'lgevamdvvmwyeylv'
